860673d0a27539882eafc1d7085cc1e3c0e7b0cd
John Lin <bigjohnlin@gmail.com>
2018-01-22 22:28:02 -0500

    Refactor: API call header fix

--- app/services/spoonacular.service.ts
+++ app/services/spoonacular.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import {fromObject, Observable} from "tns-core-modules/data/observable";
 import { Subject } from 'rxjs/Subject';
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams,HttpErrorResponse } from "@angular/common/http";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/do";
 import * as ts from "typescript/lib/tsserverlibrary";
@@ -15,8 +15,8 @@ export class SpoonacularService {
     private autoIngredients_ = new Subject<any>();
     public autoIngredients$ = this.autoIngredients_.asObservable();
 
-    private header = new HttpHeaders();
-    private getRecipesURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=flour";
+    public header = new HttpHeaders();
+    private getRecipesURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients";
     private getAutoCompleteURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/autocomplete";
 
     private recipesRet: any;
@@ -25,12 +25,18 @@ export class SpoonacularService {
         /**
          * configurations for API
          */
-        this.header.append("Accept", "application/json");
-        this.header.append("X-Mashape-Key", "");  // NOTE: need your own API
-
+        this.header.set("X-Mashape-Key", "b3k8JoveF4mshGEU6P3Te6bbwB5hp1PLqkIjsnrQboitWybr3e");  // NOTE: need your own API
+        this.header.set("Accept", "application/json");
+        //console.log(this.header.get("Accept"));
         this.recipes$.subscribe((recipes) => {
             this.recipesRet = recipes;
         });
+    }
+
+    ngOnInit() {
+        this.header.set("X-Mashape-Key", "b3k8JoveF4mshGEU6P3Te6bbwB5hp1PLqkIjsnrQboitWybr3e");  // NOTE: need your own API
+        this.header.set("Accept", "application/json");
+        console.log(this.header.get("Accept"));
     }
 
     getAutoComplete(intolerances, queryStr) {
@@ -50,9 +56,26 @@ export class SpoonacularService {
         const params = new HttpParams(
             {fromString: clientParams}
         );
-        this.http.get(this.getRecipesURL, { params: params }).subscribe((result) => {
+        let headers = new HttpHeaders().set("X-Mashape-Key", "b3k8JoveF4mshGEU6P3Te6bbwB5hp1PLqkIjsnrQboitWybr3e").set("Accept", "application/json");;  // NOTE: need your own API
+        console.log("params " + params);
+        //console.log(headers.get("X-Mashape-Key"));
+        this.http.get(this.getRecipesURL, { params: params, headers : this.header, observe : "response" }).subscribe(result => {
             console.log(result);
             this.recipes_.next(result);
+            console.log(result.headers.get("X-Mashape-Key"));
+        }, (err: HttpErrorResponse) => {
+            if (err.error instanceof Error) {
+                // A client-side or network error occurred. Handle it accordingly.
+                console.log('An error occurred:', err.error.message);
+            } else {
+                // The backend returned an unsuccessful response code.
+                // The response body may contain clues as to what went wrong,
+                console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+                console.log(err.headers.get("Accept"));
+                for (var property in err.error) {
+                    console.log(property + "=" + err.error[property]);
+                }
+            }
         });
     }
 

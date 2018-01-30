cb4c73f8bca650eca1f28a040cf42e09a124280a
Unknown <wysglby@gmail.com>
2018-01-30 14:28:35 -0500

    basic UI components finished for profile page
    i finished the major ui components for profile. still need to add link to chart pic (needs that to be created first by another teammember) also still need to add click to expand bar.

--- app/components/userProfile.component.ts
+++ app/components/userProfile.component.ts
@@ -1,11 +1,24 @@
 import {Component} from "@angular/core";
+import {EventData} from "tns-core-modules/data/observable";
+import {SpoonacularService} from "../services/spoonacular.service";
+import {SearchBar} from "tns-core-modules/ui/search-bar";
+import {RouterExtensions} from "nativescript-angular";
 
 @Component({
     selector: "user-profile",
     moduleId: module.id,
-    templateUrl: "./userProfile.component.html"
+    templateUrl: "./userProfile.component.html",
+    styleUrls: ['./userProfile.component.css']
 })
 export class UserProfileComponent {
+    // although there is only one pic for now. i still make this a list
+    // so later on the user may save multiple pictures
+    private usr_pic = [
+        {image: '~/res/usr_image.jpg'}];
+
     constructor() {
+        
     }
+
+    
 }

14ddc47e016afbbdf59f899eeecc78c668d5bc6d
Unknown <wysglby@gmail.com>
2018-03-03 00:50:33 -0500

    comment page basic structure finished
    basic look is done. still need to add the comment box.

--- app/components/commentPage.component.ts
+++ app/components/commentPage.component.ts
@@ -1,6 +1,5 @@
 import {Component, ViewChild, ElementRef} from "@angular/core";
 import {FirebaseUserService} from "../services/firebaseUser.service";
-
 @Component({
     selector: "comment-recipe",
     moduleId: module.id,
@@ -9,8 +8,13 @@ import {FirebaseUserService} from "../services/firebaseUser.service";
 })
 
 export class CommentingRecipeComponent {
+    private name = "Angela England";
+    private comment = "I love this recipe, I think this is a really really really amazing recipe!! Would love to see more recipes liek this one. love this recipe, I think this is a really really really amazing recipe!! Would love to see more recipes liek this one.";
+    // comment
+
     constructor() {
     }
+
 
 
 }

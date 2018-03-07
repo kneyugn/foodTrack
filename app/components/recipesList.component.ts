d13ebd633f9f17a598c6fdfbeb0146de5a7dfa2e
kim nguyen <nguyen.thi.kd@gmail.com>
2018-03-07 15:13:41 -0500

    Fix: Reset input after adding w button

--- app/components/recipesList.component.ts
+++ app/components/recipesList.component.ts
@@ -12,6 +12,7 @@ export class RecipesListComponent {
 
     private recipeLists: {title:string}[] = [{title:"Fine Dining"}, {title:"Quick Dishes"}];
     private listName: string = "";
+    private inputBox: any = null;
 
     constructor() {
     }    
@@ -20,18 +21,20 @@ export class RecipesListComponent {
         let textField = <TextField>args.object;
         if(textField.text != "") {
             this.recipeLists.push({title:textField.text});
+            textField.text = "";
         }
-        textField.text = "";
     }
 
     onTextChange(args) {
         let textField = <TextField>args.object;
         this.listName = textField.text;
+        this.inputBox = textField;
     }
 
     saveResponse(args) {
         if(this.listName != "") {
             this.recipeLists.push({title:this.listName});
+            this.inputBox.text = "";
         }
     }
 

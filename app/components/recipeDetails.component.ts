a28eaf503b9a39669ce2d2015e435fbdf477fc1b
Zihan Xu <zihan.alex.xu@gmail.com>
2018-03-09 20:01:02 -0500

    [ADD] Rendered ingrendients and directions on recipe details page

--- app/components/recipeDetails.component.ts
+++ app/components/recipeDetails.component.ts
@@ -1,28 +1,59 @@
-import { Component, OnInit} from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy} from "@angular/core";
 import {FirebaseRecipeService} from "../services/firebaseRecipe.service";
 import {RouterExtensions} from "nativescript-angular";
 import {ViewChild, ElementRef} from "@angular/core";
+import { SegmentedBar, SegmentedBarItem } from "ui/segmented-bar";
 
 @Component({
     selector: "recipe-details",
     moduleId: module.id,
     templateUrl: "./recipeDetails.component.html",
     styleUrls: ['./recipeDetails.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class RecipeDetailsComponent implements OnInit {
     private recipe;
+    private recipe_name;
+    private food_pic;
     private value = 5;
+    private max = 5;
+    private myItems: Array<SegmentedBarItem>;
+    private segSelectedIndex: number = 0;
+    private ingredients=[];
+    private directions=[];
+
     @ViewChild('starValue') starValue: ElementRef;
     constructor(private recipeService: FirebaseRecipeService, private routerExtensions: RouterExtensions) {
         this.recipeService.recipe$.subscribe((detailedRecipe) => {
             this.recipe = detailedRecipe;
-            console.log("from recipes", JSON.stringify(detailedRecipe));
+            // console.log("from recipes", JSON.stringify(detailedRecipe));
         })
-        
+
+        this.myItems = [];
+        const item1 = new SegmentedBarItem();
+        item1.title = "Ingredients";
+        this.myItems.push(item1);
+        const item2 = new SegmentedBarItem();
+        item2.title = "Directions";
+        this.myItems.push(item2);
     }
     ngOnInit() {
-        this.calculateAvg();
+        this.initName();
+        this.displayPic();
+        // this.calculateAvg();
+        this.initIngredients();
+        this.initDirections();
+    }
+
+    initName() {
+        this.recipe_name = this.recipe.title;
+    }
+
+    displayPic() {
+        this.food_pic = this.recipe.image;
+        // console.log(this.food_pic)
+
     }
 
     // TODO: update this function after john? fixes the id problem
@@ -36,11 +67,24 @@ export class RecipeDetailsComponent implements OnInit {
         
     }
 
-
     unchange(avg) {
         this.value = avg;
         this.starValue.nativeElement.value = avg;
         // console.log(this.starValue.nativeElement.value);
 
+    }
+
+    onSelectedIndexChange(args) {
+        let segmetedBar = <SegmentedBar>args.object;
+        this.segSelectedIndex = segmetedBar.selectedIndex;
+    }
+
+    initIngredients() {
+        this.ingredients = this.recipe.extendedIngredients;
+        console.log("from ingredients", JSON.stringify(this.ingredients[0].originalString));
+    }
+
+    initDirections() {
+        this.directions = this.recipe.cooking_directions;
     }
 }

80534662f4cd20a57e777bd7c42a634f3e2158aa
Jeremy Winter <jeremydwinter@gmail.com>
2018-03-28 19:58:09 -0400

    [ADD] Created Notifications UI

--- app/components/notifications.component.ts
+++ app/components/notifications.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import {FirebaseUserService} from "../services/firebaseUser.service";
+import {RouterExtensions} from "nativescript-angular";
+import {FirebaseRecipeService} from "../services/firebaseRecipe.service";
 
 @Component({
     selector: "notifications",
@@ -8,4 +11,16 @@ import { Component } from "@angular/core";
 })
 
 export class NotificationsComponent {
+
+    private notifications: string[];
+
+    constructor(private userService: FirebaseUserService,
+        private recipeService: FirebaseRecipeService,
+        private routerExtensions: RouterExtensions) {
+        this.notifications = ["test notification", "another one that goes on for quite a while, as notifications are wont to do"];
+    }
+
+    removeNotification(i: number) {
+        this.notifications.splice(i, 1);
+    }
 }

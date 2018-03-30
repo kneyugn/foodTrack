d20a31034246c5b8723b4575a0f0ab53924a4253
Zihan Xu <zihan.alex.xu@gmail.com>
2018-03-30 15:18:57 -0400

    [ADD] UI for login and registration page

---
+++ app/components/userRegistration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "registration",
+    moduleId: module.id,
+    templateUrl: "./userRegistration.component.html",
+    styleUrls: ['./userRegistration.component.css', "./css/icons.css"],
+})
+
+export class RegistrationComponent {
+    
+}
